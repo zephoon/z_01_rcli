7acf8452903073d72f1d5627616b6d48c6520bc2
zephoon <zephoonyu@protonmail.com>
2024-04-23 23:37:12 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+// rcli csv -i input.csv -o output.json
+use anyhow::Result;
+use clap::Parser;
+
+use rcli::{process_csv, Opts, SubCommand};
+
+fn main() -> Result<()> {
+    let opts: Opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(csv_opts) => process_csv(&csv_opts.input, &csv_opts.output)?,
+    }
+    Ok(())
 }
